149c40c02c3fa64b62bd68dc0a72c9f79480de5b
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-03-12 19:01:58 -0300

    ajustando rota Edit para o admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -2,4 +2,11 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
     @admins = Admin.all    #serve pra listar all admins
   end
+
+
+
+  def edit
+ @admin = Admin.find(params [:id])
+  end
+
 end
