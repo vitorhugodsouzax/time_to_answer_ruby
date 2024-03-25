b36430acdb6401450790835dff6fd5e2412ba25d
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-03-25 17:13:07 -0300

    criei as migration para as questoes

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,10 +1,9 @@
-User
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         # Kaminari Pagination
-         paginates_per 5
+# Kaminari Pagination
+paginates_per 10
 end
