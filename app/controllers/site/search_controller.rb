4675de09203f28341648ee67163283652e8f2199
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-04-04 17:03:12 -0300

    fazendo a barra de pesquisa funcionar

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,12 @@
 class Site::SearchController < SiteController
+    # Esta classe de controlador lida com pesquisas no site
+  
+    # Método para buscar perguntas
     def questions
-    @questions = Question.includes(:answers).page(params[:page])
+      # Carrega as perguntas com suas respostas associadas
+      @questions = Question.includes(:answers)  # Carrega as perguntas pré-carregando suas respostas para evitar consultas adicionais
+                           .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")  # Filtra perguntas cuja descrição contenha o termo de pesquisa, ignorando maiúsculas e minúsculas
+                           .page(params[:page])  # Pagina os resultados para exibição em páginas
     end
-   end
+  end
+  
