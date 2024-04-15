ecb730efbe4123e5e0c214df1c9ed4874ff49153
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-04-15 18:25:25 -0300

    railsdb:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_09_201919) do
+ActiveRecord::Schema.define(version: 2024_04_15_210654) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 2024_04_09_201919) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "questions_count"
+  end
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "address"
+    t.string "gender"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
